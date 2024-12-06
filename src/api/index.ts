20da1125aeda78fe98dbeeb82fab9e9c74458bea
Hyre <developer.hyre@gmail.com>
2024-12-06 05:23:29 -0500

    .

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { User, Project, Task, TaskHistory } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://pms-basic.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
